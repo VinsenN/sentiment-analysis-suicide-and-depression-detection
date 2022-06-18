de025c3fc30cc108929aa1a17a454a84c9febabe
Vinsen Nawir <vinsennawir@gmail.com>
2022-06-18 22:52:01 +0800

    update and changes some stuffs

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, render_template
 
 from models.preprocess import preprocess_all
-from models.sklearn_model import SKLearn_Model
 import pickle
 
 app = Flask(__name__)
-class_model = pickle.load(open('models/svm_tfidf.obj','rb'))
+
+feature_extraction = pickle.load(open(r'models/tfidf_transfomer.pkl', 'rb'))
+model = pickle.load(open(r'models/svm_tfidf.sav', 'rb'))
 
 @app.route('/', methods=['GET'])
 def show_html():
@@ -21,8 +22,11 @@ def predict():
         return render_template('index.html')
 
     filtered_message = preprocess_all(message)
-    result = class_model.predict(filtered_message)
+    vectorized_message = feature_extraction.transform([message])
+
+    result = model.predict(vectorized_message)[0]
     return render_template('index.html', predicted_message = message.strip(), prediction = result)
+
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
